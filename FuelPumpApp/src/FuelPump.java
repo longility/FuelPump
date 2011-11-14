0d26d4a4c1901d7de6605bd1fd72f8cb6997c9c9
longility <long.mai@longility.com>
2011-11-14 15:44:35 -0600

    Session 2

---
+++ FuelPumpApp/src/FuelPump.java
@@ -0,0 +1,22 @@
+
+public class FuelPump {
+	
+	public void authorizePump(double authorizedAmount) {
+		//Reset gallonsPumped
+		//Set authorizedAmount.
+	}
+	
+	public void turnOn(String fuelGrade) {
+		//turn fuel pump on
+		//set fuel type to the given fuel grade input
+	}
+	
+	public void pumpFuel() {
+		//If the fuel pump is turned on, then
+			//transfer fuel from tank to gallonsPumped, up to the authorized amount or when out of fuel
+	}
+	
+	public void turnOff() { 
+		//turn fuel pump off
+	}
+}
---
+++ FuelPumpApp/src/FuelType.java
@@ -0,0 +1,17 @@
+
+public class FuelType {
+	private double fuelPrice;
+	private double gallonsLeft;
+	
+	public double getFuelPrice() {
+		return fuelPrice;
+	}
+	
+	public double getGallonsLeft() {
+		return gallonsLeft;
+	}
+	
+	public void drainGallons(double gallons) {
+		
+	}
+}
